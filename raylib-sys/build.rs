e69302845c94010b8f990cac4964b1297e964f29
IoIxD <alphaproject217@gmail.com>
2024-03-28 16:20:44 -0700

    build: wasm building requires -O3 to be set in EMCC_CFLAGS, apperently. see emscripten-core/emscripten#19346

--- raylib-sys/build.rs
+++ raylib-sys/build.rs
@@ -306,7 +306,7 @@ fn main() {
                     {"Paste this before executing the command: set EMCC_CFLAGS="}
                     #[cfg(not(target_family = "windows"))]
                     {"Prefix your command with this (you may want to make a build script for obvious reasons...): EMCC_CFLAGS="}
-                },"\"-sUSE_GLFW=3 -sGL_ENABLE_GET_PROC_ADDRESS -sWASM=1 -sALLOW_MEMORY_GROWTH=1 -sWASM_MEM_MAX=512MB -sTOTAL_MEMORY=512MB -sABORTING_MALLOC=0 -sASYNCIFY -sFORCE_FILESYSTEM=1 -sASSERTIONS=1 -sERROR_ON_UNDEFINED_SYMBOLS=0 -sEXPORTED_RUNTIME_METHODS=ccallcwrap\"");
+                },"\"-O3 -sUSE_GLFW=3 -sGL_ENABLE_GET_PROC_ADDRESS -sWASM=1 -sALLOW_MEMORY_GROWTH=1 -sWASM_MEM_MAX=512MB -sTOTAL_MEMORY=512MB -sABORTING_MALLOC=0 -sASYNCIFY -sFORCE_FILESYSTEM=1 -sASSERTIONS=1 -sERROR_ON_UNDEFINED_SYMBOLS=0 -sEXPORTED_RUNTIME_METHODS=ccallcwrap\"");
             } else {
                 panic!("\nError regarding EMCC_CFLAGS: {:?}\n", e);
             }
