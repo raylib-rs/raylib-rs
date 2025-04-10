8db1264810c71c5dff2ed6d1813cba5af422e28e
Henry Wilder <74995093+HenryWilder@users.noreply.github.com>
2025-04-10 16:25:45 -0400

    Create FilePathIter and create initial tests

--- raylib/src/core/file.rs
+++ raylib/src/core/file.rs
@@ -4,6 +4,61 @@ use crate::ffi;
 use crate::core::RaylibHandle;
 use std::ffi::{CStr, CString, OsString};
 
+/// Iterator should not outlive list
+/// ```compile_fail
+/// let mut it;
+/// {
+///     let list = ManuallyDrop::new(FilePathList(ffi::FilePathList {
+///         capacity: 1,
+///         count: 1,
+///         paths: std::ptr::null_mut(),
+///     }));
+///     it = list.iter();
+/// }
+/// let _f = it.next(); // error: `list` is no longer in scope
+/// ```
+pub struct FilePathIter<'a> {
+    iter: std::slice::Iter<'a, Option<&'a std::ffi::c_char>>,
+}
+impl<'a> FilePathIter<'a> {
+    fn new(list: *mut *mut std::ffi::c_char, count: u32) -> Self {
+        // No new items are being created that get dropped here, these are just changes in perspective of how to borrow-check the pointers.
+        assert!(!list.is_null(), "file path pointer cannot be null");
+        assert!(list.is_aligned(), "file path pointer must be aligned");
+        let list = list.cast::<Option<&'a std::ffi::c_char>>();
+        let iter = unsafe { std::slice::from_raw_parts(list, count as usize) }.iter();
+        Self { iter }
+    }
+    fn convert_item(f: &Option<&'a std::ffi::c_char>) -> &'a str {
+        // CStr isn't being "constructed", it's essentially an adapter on &[c_char]
+        let s = f.map(std::slice::from_ref).unwrap();
+        unsafe { CStr::from_ptr(s.as_ptr()) }.to_str().unwrap()
+    }
+}
+impl<'a> Iterator for FilePathIter<'a> {
+    type Item = &'a str;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.iter.next().map(Self::convert_item)
+    }
+
+    #[inline]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.iter.size_hint()
+    }
+}
+impl<'a> DoubleEndedIterator for FilePathIter<'a> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        self.iter.next_back().map(Self::convert_item)
+    }
+}
+impl<'a> ExactSizeIterator for FilePathIter<'a> {
+    #[inline]
+    fn len(&self) -> usize {
+        self.iter.len()
+    }
+}
+
 make_thin_wrapper!(FilePathList, ffi::FilePathList, ffi::UnloadDirectoryFiles);
 make_thin_wrapper!(
     DroppedFilePathList,
@@ -28,6 +83,10 @@ impl FilePathList {
             .map(|f| unsafe { CStr::from_ptr(*f) }.to_str().unwrap())
             .collect()
     }
+    /// An iterator over the paths held in this list.
+    pub fn iter(&self) -> FilePathIter<'_> {
+        FilePathIter::new(self.0.paths, self.count())
+    }
 }
 
 impl DroppedFilePathList {
@@ -46,6 +105,10 @@ impl DroppedFilePathList {
             .iter()
             .map(|f| unsafe { CStr::from_ptr(*f) }.to_str().unwrap())
             .collect()
+    }
+    /// An iterator over the paths held in this list.
+    pub fn iter(&self) -> FilePathIter<'_> {
+        FilePathIter::new(self.0.paths, self.count())
     }
 }
 
@@ -135,5 +198,109 @@ impl RaylibHandle {
     /// Check if a file has been dropped into window
     pub fn load_dropped_files(&self) -> DroppedFilePathList {
         unsafe { DroppedFilePathList(ffi::LoadDroppedFiles()) }
+    }
+}
+
+#[cfg(test)]
+mod file_path_iter_tests {
+    use std::mem::ManuallyDrop;
+    use super::*;
+
+    #[test]
+    #[should_panic]
+    fn test_null_list() {
+        let list = ManuallyDrop::new(FilePathList(ffi::FilePathList {
+            capacity: 0,
+            count: 0,
+            paths: std::ptr::null_mut(),
+        }));
+        let _it = list.iter();
+        // should have panicked while calling .iter()
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_null_item() {
+        let mut paths = [std::ptr::null_mut()];
+        let list = ManuallyDrop::new(FilePathList(ffi::FilePathList {
+            capacity: 1,
+            count: 1,
+            paths: paths.as_mut_ptr(),
+        }));
+        let mut it = list.iter();
+        let _f = it.next();
+        // should have panicked while calling .next()
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_null_item_double_ended() {
+        let mut paths = [std::ptr::null_mut()];
+        let list = ManuallyDrop::new(FilePathList(ffi::FilePathList {
+            capacity: 1,
+            count: 1,
+            paths: paths.as_mut_ptr(),
+        }));
+        let mut it = list.iter();
+        let _f = it.next_back();
+        // should have panicked while calling .next_back()
+    }
+
+    #[test]
+    fn test_len() {
+        let mut paths = [
+            CStr::from_bytes_with_nul(b"apple\0").unwrap().as_ptr().cast_mut(),
+            CStr::from_bytes_with_nul(b"orange\0").unwrap().as_ptr().cast_mut(),
+            CStr::from_bytes_with_nul(b"banana\0").unwrap().as_ptr().cast_mut(),
+            CStr::from_bytes_with_nul(b"mango\0").unwrap().as_ptr().cast_mut(),
+            CStr::from_bytes_with_nul(b"pineapple\0").unwrap().as_ptr().cast_mut(),
+        ];
+        let list = ManuallyDrop::new(FilePathList(ffi::FilePathList {
+            capacity: 5,
+            count: 5,
+            paths: paths.as_mut_ptr(),
+        }));
+        let mut it = list.iter();
+        assert_eq!(it.len(), 5);
+        assert_eq!(it.next(), Some("apple"));
+        assert_eq!(it.len(), 4);
+        assert_eq!(it.next(), Some("orange"));
+        assert_eq!(it.len(), 3);
+        assert_eq!(it.next(), Some("banana"));
+        assert_eq!(it.len(), 2);
+        assert_eq!(it.next(), Some("mango"));
+        assert_eq!(it.len(), 1);
+        assert_eq!(it.next(), Some("pineapple"));
+        assert_eq!(it.len(), 0);
+        assert_eq!(it.next(), None);
+    }
+
+    #[test]
+    fn test_len_double_ended() {
+        let mut paths = [
+            CStr::from_bytes_with_nul(b"apple\0").unwrap().as_ptr().cast_mut(),
+            CStr::from_bytes_with_nul(b"orange\0").unwrap().as_ptr().cast_mut(),
+            CStr::from_bytes_with_nul(b"banana\0").unwrap().as_ptr().cast_mut(),
+            CStr::from_bytes_with_nul(b"mango\0").unwrap().as_ptr().cast_mut(),
+            CStr::from_bytes_with_nul(b"pineapple\0").unwrap().as_ptr().cast_mut(),
+        ];
+        let list = ManuallyDrop::new(FilePathList(ffi::FilePathList {
+            capacity: 5,
+            count: 5,
+            paths: paths.as_mut_ptr(),
+        }));
+        let mut it = list.iter();
+        assert_eq!(it.len(), 5);
+        assert_eq!(it.next_back(), Some("pineapple"));
+        assert_eq!(it.len(), 4);
+        assert_eq!(it.next_back(), Some("mango"));
+        assert_eq!(it.len(), 3);
+        assert_eq!(it.next_back(), Some("banana"));
+        assert_eq!(it.len(), 2);
+        assert_eq!(it.next_back(), Some("orange"));
+        assert_eq!(it.len(), 1);
+        assert_eq!(it.next_back(), Some("apple"));
+        assert_eq!(it.len(), 0);
+        assert_eq!(it.next_back(), None);
     }
 }
