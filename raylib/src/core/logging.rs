e7cdd4ddc317058e1258ef46b7cd8226804a8aa2
Amy Wilder <henrythepony@gmail.com>
2025-07-26 16:17:20 -0400

    Cleanup logging.rs

--- raylib/src/core/logging.rs
+++ raylib/src/core/logging.rs
@@ -1,4 +1,5 @@
-///! Functions to change the behavior of raylib logging.
+//! Functions to change the behavior of raylib logging.
+
 // TODO: refactor this entire thing to use log
 use crate::consts::TraceLogLevel;
 use crate::ffi;
@@ -8,15 +9,19 @@ use std::ffi::CString;
 #[inline]
 pub fn set_trace_log(types: TraceLogLevel) {
     unsafe {
-        ffi::SetTraceLogLevel((types as u32) as i32);
+        ffi::SetTraceLogLevel(types as i32);
     }
 }
 
-/// Writes a trace log message (`Log::INFO`, `Log::WARNING`, `Log::ERROR`, `Log::DEBUG`).
+/// Writes a trace log message ([`LOG_INFO`](TraceLogLevel::LOG_INFO), [`LOG_WARNING`](TraceLogLevel::LOG_WARNING), [`LOG_ERROR`](TraceLogLevel::LOG_ERROR), [`LOG_DEBUG`](TraceLogLevel::LOG_DEBUG)).
+///
+/// # Panics
+///
+/// This function will panic if `text` contains an internal 0 byte.
 #[inline]
 pub fn trace_log(msg_type: TraceLogLevel, text: &str) {
     unsafe {
-        let text = CString::new(text).unwrap();
-        ffi::TraceLog((msg_type as u32) as i32, text.as_ptr());
+        let text = CString::new(text).expect("text should not contain an internal 0 byte");
+        ffi::TraceLog(msg_type as i32, text.as_ptr());
     }
 }
