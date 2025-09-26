780d3a26b631fc17323265484caa79f5d737fc7d
iann <lanur622@gmail.com>
2025-09-27 03:57:39 +0900

    simple fix for failing screenshot test (with TODO for next raylib release

--- raylib-test/src/misc.rs
+++ raylib-test/src/misc.rs
@@ -7,7 +7,10 @@ mod core_test {
         let mut handle = TEST_HANDLE.write().unwrap();
         let rl = handle.as_mut().unwrap();
         rl.take_screenshot(t, "test_out/screenshot.png");
-        assert!(std::path::Path::new("test_out/screenshot.png").exists());
+        //TODO: allows paths upon next raylib release update https://github.com/raysan5/raylib/commit/909c83fd4ad50062b977ca8dfa48273eb2376da4
+        // assert!(std::path::Path::new("test_out/screenshot.png").exists());
+        assert!(std::path::Path::new("screenshot.png").exists());
+
     }
 
     ray_test!(test_screendata);
