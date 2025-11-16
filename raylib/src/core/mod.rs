8b2d23607fa8f41770af8e669c72a6107e73aa57
Amy Wilder <henrythepony@gmail.com>
2025-11-16 12:31:33 -0500

    Add missing config flags to `RaylibBuilder`
    also mark setters `const` and change `RaylibBuilder` to accept `&str` instead of `String` to save an unnecessary allocation

--- raylib/src/core/mod.rs
+++ raylib/src/core/mod.rs
@@ -82,94 +82,164 @@ impl Drop for RaylibHandle {
 
 /// A builder that allows more customization of the game window shown to the user before the `RaylibHandle` is created.
 #[derive(Debug, Default)]
-pub struct RaylibBuilder {
+pub struct RaylibBuilder<'a> {
     fullscreen_mode: bool,
     window_resizable: bool,
     window_undecorated: bool,
     window_transparent: bool,
     msaa_4x_hint: bool,
     vsync_hint: bool,
+    window_hidden: bool,
+    window_always_run: bool,
+    window_minimized: bool,
+    window_maximized: bool,
+    window_unfocused: bool,
+    window_topmost: bool,
+    window_highdpi: bool,
+    window_mouse_passthrough: bool,
+    borderless_windowed_mode: bool,
+    interlaced_hint: bool,
     log_level: TraceLogLevel,
     width: i32,
     height: i32,
-    title: String,
+    title: &'a str,
 }
 #[inline]
 #[must_use]
 /// Creates a `RaylibBuilder` for choosing window options before initialization.
-pub fn init() -> RaylibBuilder {
+pub fn init<'a>() -> RaylibBuilder<'a> {
     RaylibBuilder {
         width: 640,
         height: 480,
-        title: "raylib-rs".to_string(),
+        title: "raylib-rs",
         ..Default::default()
     }
 }
 
-impl RaylibBuilder {
+impl<'a> RaylibBuilder<'a> {
     /// Sets the window to be fullscreen.
-    pub fn fullscreen(&mut self) -> &mut Self {
+    pub const fn fullscreen(&mut self) -> &mut Self {
         self.fullscreen_mode = true;
         self
     }
 
     /// Set the builder's log level.
-    pub fn log_level(&mut self, level: TraceLogLevel) -> &mut Self {
+    pub const fn log_level(&mut self, level: TraceLogLevel) -> &mut Self {
         self.log_level = level;
         self
     }
     /// Sets the window to be resizable.
-    pub fn resizable(&mut self) -> &mut Self {
+    pub const fn resizable(&mut self) -> &mut Self {
         self.window_resizable = true;
         self
     }
 
     /// Sets the window to be undecorated (without a border).
-    pub fn undecorated(&mut self) -> &mut Self {
+    pub const fn undecorated(&mut self) -> &mut Self {
         self.window_undecorated = true;
         self
     }
 
     /// Sets the window to be transparent.
-    pub fn transparent(&mut self) -> &mut Self {
+    pub const fn transparent(&mut self) -> &mut Self {
         self.window_transparent = true;
         self
     }
 
     /// Hints that 4x MSAA (anti-aliasing) should be enabled. The system's graphics drivers may override this setting.
-    pub fn msaa_4x(&mut self) -> &mut Self {
+    pub const fn msaa_4x(&mut self) -> &mut Self {
         self.msaa_4x_hint = true;
         self
     }
 
     /// Hints that vertical sync (VSync) should be enabled. The system's graphics drivers may override this setting.
-    pub fn vsync(&mut self) -> &mut Self {
+    pub const fn vsync(&mut self) -> &mut Self {
         self.vsync_hint = true;
         self
     }
 
+    /// Set to hide window
+    pub const fn hidden(&mut self) -> &mut Self {
+        self.window_hidden = true;
+        self
+    }
+
+    /// Set to allow windows running while minimized
+    pub const fn always_run(&mut self) -> &mut Self {
+        self.window_always_run = true;
+        self
+    }
+
+    /// Set to minimize window (iconify)
+    pub const fn minimized(&mut self) -> &mut Self {
+        self.window_minimized = true;
+        self
+    }
+
+    /// Set to maximize window (expanded to monitor)
+    pub const fn maximized(&mut self) -> &mut Self {
+        self.window_maximized = true;
+        self
+    }
+
+    /// Set to window non focused
+    pub const fn unfocused(&mut self) -> &mut Self {
+        self.window_unfocused = true;
+        self
+    }
+
+    /// Set to window always on top
+    pub const fn topmost(&mut self) -> &mut Self {
+        self.window_topmost = true;
+        self
+    }
+
+    /// Set to support HighDPI
+    pub const fn highdpi(&mut self) -> &mut Self {
+        self.window_highdpi = true;
+        self
+    }
+
+    /// Set to support mouse passthrough, only supported when [`Self::undecorated`]
+    pub const fn mouse_passthrough(&mut self) -> &mut Self {
+        self.window_mouse_passthrough = true;
+        self
+    }
+
+    /// Set to run program in borderless windowed mode
+    pub const fn borderless_windowed_mode(&mut self) -> &mut Self {
+        self.borderless_windowed_mode = true;
+        self
+    }
+
+    /// Set to try enabling interlaced video format (for V3D)
+    pub const fn interlaced_hint(&mut self) -> &mut Self {
+        self.interlaced_hint = true;
+        self
+    }
+
     /// Sets the window's width.
-    pub fn width(&mut self, w: i32) -> &mut Self {
+    pub const fn width(&mut self, w: i32) -> &mut Self {
         self.width = w;
         self
     }
 
     /// Sets the window's height.
-    pub fn height(&mut self, h: i32) -> &mut Self {
+    pub const fn height(&mut self, h: i32) -> &mut Self {
         self.height = h;
         self
     }
 
     /// Sets the window's width and height.
-    pub fn size(&mut self, w: i32, h: i32) -> &mut Self {
+    pub const fn size(&mut self, w: i32, h: i32) -> &mut Self {
         self.width = w;
         self.height = h;
         self
     }
 
     /// Sets the window title.
-    pub fn title(&mut self, text: &str) -> &mut Self {
-        self.title = text.to_string();
+    pub const fn title(&mut self, text: &'a str) -> &mut Self {
+        self.title = text;
         self
     }
 
@@ -190,8 +260,35 @@ impl RaylibBuilder {
         if self.window_undecorated {
             flags |= FLAG_WINDOW_UNDECORATED as u32;
         }
+        if self.window_hidden {
+            flags |= FLAG_WINDOW_HIDDEN as u32;
+        }
+        if self.window_minimized {
+            flags |= FLAG_WINDOW_MINIMIZED as u32;
+        }
+        if self.window_maximized {
+            flags |= FLAG_WINDOW_MAXIMIZED as u32;
+        }
+        if self.window_unfocused {
+            flags |= FLAG_WINDOW_UNFOCUSED as u32;
+        }
+        if self.window_topmost {
+            flags |= FLAG_WINDOW_TOPMOST as u32;
+        }
+        if self.window_always_run {
+            flags |= FLAG_WINDOW_ALWAYS_RUN as u32;
+        }
         if self.window_transparent {
             flags |= FLAG_WINDOW_TRANSPARENT as u32;
+        }
+        if self.window_highdpi {
+            flags |= FLAG_WINDOW_HIGHDPI as u32;
+        }
+        if self.window_mouse_passthrough {
+            flags |= FLAG_WINDOW_MOUSE_PASSTHROUGH as u32;
+        }
+        if self.borderless_windowed_mode {
+            flags |= FLAG_BORDERLESS_WINDOWED_MODE as u32;
         }
         if self.msaa_4x_hint {
             flags |= FLAG_MSAA_4X_HINT as u32;
@@ -199,16 +296,19 @@ impl RaylibBuilder {
         if self.vsync_hint {
             flags |= FLAG_VSYNC_HINT as u32;
         }
+        if self.interlaced_hint {
+            flags |= FLAG_INTERLACED_HINT as u32;
+        }
 
         unsafe {
-            ffi::SetConfigFlags(flags as u32);
+            ffi::SetConfigFlags(flags);
         }
 
         unsafe {
             ffi::SetTraceLogLevel(self.log_level as i32);
         }
 
-        let rl = init_window(self.width, self.height, &self.title);
+        let rl = init_window(self.width, self.height, self.title);
 
         (rl, RaylibThread(PhantomData))
     }
